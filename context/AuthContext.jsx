f05c29984032c84c6bbde95ff2f19f4ac3d3ebb8
Saurabh <ss6408367@gmail.com>
2023-06-21 18:45:02 +0530

    added google signin

--- context/AuthContext.jsx
+++ context/AuthContext.jsx
@@ -1,52 +1,60 @@
-import {createContext,useContext,useEffect} from 'react'
-import {onAuthStateChanged,createUserWithEmailAndPassword,signInWithEmailAndPassword, signOut} from "firebase/auth"
-import { auth } from '../config/firebase';
-import { useState } from 'react';
-
+import { createContext, useContext, useEffect } from "react";
+import {
+  onAuthStateChanged,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+import { auth } from "../config/firebase";
+import { useState } from "react";
 
 const AuthContext = createContext({});
 
 export const useAuth = () => useContext(AuthContext);
 
-export function  AuthContextProvider({children}){
-    const [user,setUser] =useState(null);
-    const [loading,setLoading] = useState(true);
-       useEffect(()=>{
-       const unsubscribe = onAuthStateChanged(auth,(user)=>{ //this is the information about
-        //everytime user will change
-            if(user){
-                setUser({
-                   uid:user.uid,
-                   email:user.email,
-                   displayName:user.displayName,
+export function AuthContextProvider({ children }) {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      //this is the information about
+      //everytime user will change
+      if (user) {
+        setUser({
+          uid: user.uid,
+          email: user.email,
+          displayName: user.displayName,
+        });
+      } else {
+        setUser(null);
+      }
+      setLoading(false);
+    });
 
-                })
-            }else{
-                setUser(null);
-            }
-            setLoading(false);
-       })
+    return () => unsubscribe();
+  }, []);
+  function googleSignin() {
+    const googleAuthProvider = new GoogleAuthProvider();
+    return signInWithPopup(auth, googleAuthProvider);
+  }
+  const signup = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
 
-       return ()=> unsubscribe();
-       },[]);
+  const login = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
-         const signup = (email,password) =>{
-           return createUserWithEmailAndPassword(auth,email,password)
-         }
+  const logout = async () => {
+    setUser(null);
+    await signOut(auth);
+  };
 
-         const login = (email,password) =>{
-            return signInWithEmailAndPassword(auth,email,password)
-          }
-
-          const logout= async () =>{
-              setUser(null)
-              await signOut(auth)
-          }
-
-
-     return  (
-     <AuthContext.Provider value={{user,signup,login,logout}}>
-          {loading ? null : children}
-     </AuthContext.Provider>
-     )
+  return (
+    <AuthContext.Provider value={{ user, signup, login, logout, googleSignin }}>
+      {loading ? null : children}
+    </AuthContext.Provider>
+  );
 }
--- pages/login.jsx
+++ pages/login.jsx
@@ -1,64 +1,112 @@
-import React,{useState} from 'react'
-import { useAuth } from '../context/AuthContext'
-import {useRouter} from 'next/router'
-import Head from "next/head"
-import Link from "next/link"
+import React, { useState } from "react";
+import { useAuth } from "../context/AuthContext";
+import { useRouter } from "next/router";
+import Head from "next/head";
+import Link from "next/link";
 
 const Login = () => {
-    const {user,login} = useAuth();
-    const router = useRouter();
-    const [data,setData] = useState({
-        email:'',
-        password:''
-    })
+  const { user, login, googleSignin } = useAuth();
+  const router = useRouter();
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
 
-    const handleLogin = async(e) =>{
-        e.preventDefault();
-        try{
-            await login(data.email,data.password)
-            router.push('./dashboard')
-        } catch(err){
-            console.log(err);
-        }
-       console.log(data);
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      await login(data.email, data.password);
+      router.push("./dashboard");
+    } catch (err) {
+      console.log(err);
     }
+    console.log(data);
+  };
+  const handleGoogle = async (e) => {
+    e.preventDefault();
+    await googleSignin();
+  };
   return (
-      <>
+    <>
       <Head>
-          <title>Login | Recipeas</title>
+        <title>Login | Recipeas</title>
       </Head>
-    <div className='max-w-[1180px] mx-auto h-screen pt-20 flex justify-center '>
-        <div className='bg-gray-400 h-[400px] pt-10 mt-28 w-80 rounded shadow-2xl shadow-slate-800'>
-        <h1 className='flex justify-center text-xl text-blue-800 font-medium'>Login</h1>
-        <form  onSubmit={handleLogin} className="">
-            <p className='text-xl text-black-600 ml-12 mt-4'>Email</p>
-            <input className='ml-12 w-52 h-8 rounded-md mt-2 p-2 outline-none' type="email" placeholder='JohnDoe@gmail.com' value={data.email} onChange={(e)=>{
+      <div className="max-w-[1180px] mx-auto h-screen pt-20 flex justify-center ">
+        <div className="bg-gray-400 h-[440px] pt-10 mt-28 w-80 rounded shadow-2xl shadow-slate-800">
+          <h1 className="flex justify-center text-xl text-blue-800 font-medium">
+            Login
+          </h1>
+          <form onSubmit={handleLogin} className="">
+            <p className="text-xl text-black-600 ml-12 mt-4">Email</p>
+            <input
+              className="ml-12 w-52 h-8 rounded-md mt-2 p-2 outline-none"
+              type="email"
+              placeholder="JohnDoe@gmail.com"
+              value={data.email}
+              onChange={(e) => {
                 setData({
-                    ...data,
-                    email:e.target.value,
-                })
-            }}/>
-            <p className='ml-12 mt-4'>Password</p>
-            <input className='ml-12 w-52 h-8 rounded-md mt-2 p-2 outline-none' type="password" placeholder='Password' value={data.password} onChange={(e)=>{
+                  ...data,
+                  email: e.target.value,
+                });
+              }}
+            />
+            <p className="ml-12 mt-4">Password</p>
+            <input
+              className="ml-12 w-52 h-8 rounded-md mt-2 p-2 outline-none"
+              type="password"
+              placeholder="Password"
+              value={data.password}
+              onChange={(e) => {
                 setData({
-                    ...data,
-                    password:e.target.value,
-                })
-            }}/>
-            <div className=' mt-6 flex justify-center '>
-            <button type='submit' className='w-20 h-10 bg-cyan-600 rounded-md'> Login</button>
+                  ...data,
+                  password: e.target.value,
+                });
+              }}
+            />
+            <div className=" mt-6 flex justify-center ">
+              <button
+                type="submit"
+                className="w-20 h-10 bg-cyan-600 rounded-md"
+              >
+                {" "}
+                Login
+              </button>
             </div>
-            <div className='flex justify-center mt-4'>
-            <h1 className='mr-2'>Dont have an account ?</h1>
-            <div className='text-blue-600 underline'>
-            <Link  href="/signup">Sign Up</Link>
+            <div className=" mt-6 flex justify-center ">
+              <button
+                type="button"
+                onClick={handleGoogle}
+                className="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2"
+              >
+                <svg
+                  className="w-4 h-4 mr-2 -ml-1"
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="fab"
+                  data-icon="google"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 488 512"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
+                  ></path>
+                </svg>
+                Sign in with Google
+              </button>
             </div>
+            <div className="flex justify-center mt-4">
+              <h1 className="mr-2">Dont have an account ?</h1>
+              <div className="text-blue-600 underline">
+                <Link href="/signup">Sign Up</Link>
+              </div>
             </div>
-        </form>
-    </div>
-    </div>
+          </form>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
